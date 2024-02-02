6d9947fa3c548f4c778f28a3ee3e3b4823629aec
jlandrews87 <155691210+jlandrews87@users.noreply.github.com>
2024-02-02 12:38:25 -0500

    Add Home Page and script
    I added the files below and am experimenting with javascript now.
    This bad boy will be functional soon.

---
+++ js/script.js
@@ -0,0 +1,23 @@
+//Experiment here. Take out comments - make this an actual deal.//
+
+const installerNames = [
+    "Chris Reed",
+    "Jim Foley",
+    "Chris Taylor",
+    "Dayann",
+    "Leno",
+    "Ernesto",
+    "Joe Nelson",
+    "Bryan Haddrill",
+    "Doug Stewart",
+    "Memmo",
+    "Frans",
+    "Chris Braga",
+    "James Gee",
+    "Radu"
+];
+
+let installerName = installerNames[4];
+console.log(installerName);
+
+console.log(installerNames);
